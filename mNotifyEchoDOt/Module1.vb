727a3f8441ba2b7c499c730ba4298ee81255f716
Mark Meadows <42901465+swodaemkram@users.noreply.github.com>
2022-12-31 17:19:09 -0500

    Add files via upload

---
+++ mNotifyEchoDOt/Module1.vb
@@ -0,0 +1,76 @@
+﻿Imports System
+Imports System.IO
+Imports System.Net
+
+Module Module1
+
+    '****************************************************************************************************
+    '************************************** Command Line arguments should look like 
+    '************************************** C:\mNotify URL "Message" AuthKey
+    '************************** Since there will be so much variaty little argument checking will be done
+    '*****************************************************************************************************
+
+    Sub Main(args As String())
+        Dim arguments = Environment.GetCommandLineArgs()
+        On Error Resume Next
+
+        'Debug Console.WriteLine(arguments(1)) 'Command Line Argument 1
+        'Debug Console.WriteLine(arguments(2)) 'Command Line Argument 2
+        'Debug Console.WriteLine(arguments(3)) 'Command Line Argument 3
+
+        arguments(2) = arguments(2).Replace(" ", "%20")
+
+        Dim CompletURL As String = arguments(1) & arguments(2) & "&" & arguments(3)
+        Console.WriteLine(CompletURL)
+
+        Dim ReturnString As String = processCCRequest(CompletURL)
+
+        'Console.WriteLine(ReturnString)
+    End Sub
+    'Public Function processCCRequest(ByVal strRequest As String) As String Changed this line to the line below 
+    Public Function processCCRequest(CompleteURL As String) As String
+        'declare the web request object and set its path to the  API
+
+
+        Dim ThisRequest As WebRequest = WebRequest.Create(CompleteURL)
+
+        'configure web request object attributes
+        ThisRequest.ContentType = "application/x-www-form-urlencoded"
+        ThisRequest.Method = "POST"
+
+        'encode the request
+        Dim Encoder As New System.Text.ASCIIEncoding
+        'Dim BytesToSend As Byte() = Encoder.GetBytes(strRequest) I Changed the line below for simplicity
+        Dim BytesToSend As Byte() = Encoder.GetBytes("")
+
+        'declare the text stream and send the request to the API
+        Dim StreamToSend As Stream = ThisRequest.GetRequestStream
+        StreamToSend.Write(BytesToSend, 0, BytesToSend.Length)
+        StreamToSend.Close()
+
+        ''ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+        ''allows for validation of SSL conversations
+        ''ServicePointManager.ServerCertificateValidationCallback = New System.Net.Security.RemoteCertificateValidationCallback(AddressOf)
+        ServicePointManager.Expect100Continue = True
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+        System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+        ''| SecurityProtocolType.Tls11 | SecurityProtocolType.Tls
+        ''var(response = WebRequest.Create("https://www.howsmyssl.com/").GetResponse())
+        ''var(body = New StreamReader(response.GetResponseStream()).ReadToEnd())
+
+
+        'Catch the response from the webrequest object
+        Dim TheirResponse As HttpWebResponse = ThisRequest.GetResponse
+
+        Dim sr As New StreamReader(TheirResponse.GetResponseStream)
+        Dim strResponse As String = sr.ReadToEnd
+
+        'Out put the string to a message box - application should parse the request instead
+        ' MsgBox(strResponse)
+
+        sr.Close()
+        Return strResponse
+    End Function
+
+
+End Module
---
+++ mNotifyEchoDOt/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("mNotifyEchoDOt")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("mNotifyEchoDOt")>
+<Assembly: AssemblyCopyright("Copyright © HP 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5e439326-7b60-4cb8-b664-c466f5c2d01b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
